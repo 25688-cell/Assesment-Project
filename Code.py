b3d19ad93d2c0d35f0b9687fc9d7ccae0e992487
25688-cell <25688@burnside.school.nz>
2026-05-18 14:39:55 +1200

    Final version

--- Code.py
+++ Code.py
@@ -7,16 +7,15 @@ adding_country = "INSERT INTO Country (country_code, country_name) VALUES (?, ?)
 adding_medals = "INSERT INTO Medals (country_code, gold, silver, bronze) VALUES (?, ?, ?, ?);"
 deleting_country = "DELETE FROM Country WHERE country_code = ?;"
 deleting_medals = "DELETE FROM Medals WHERE country_code = ?;"
-finding = "SELECT * FROM Country, Medals WHERE Country.country_code = ?;"
+finding_country = "SELECT * FROM Country, Medals WHERE Country.country_code = ?;"
 # -----------------------------------------------------
 db = sqlite3.connect(database)
 cursor = db.cursor()
-cursor.execute(connecting)
+cursor.execute(connecting) # Joined the two tables together and ordered them
 result = cursor.fetchall()
-# Joined the two tables together and ordered them
 print("Welcome to the Olympic Medal Table! What would you like to do?")
 # FUNCTIONS
-def adding(): #Used to add a country to the medal table
+def adding_country(): #Used to add a country to the medal table
     country_code = input("Country code: ").upper()
     country_name = input("Country name: ")
     if not country_code.isalpha() or len(country_code) != 3 or not country_name.isalpha():
@@ -28,21 +27,21 @@ def adding(): #Used to add a country to the medal table
         bronze = int(input("Number of bronze medals: "))
     except ValueError:
         print("Invalid input. Please enter a number")
-        continue
+        return
     #Inserting country and medals inside the tables
     cursor.execute(adding_country, (country_code, country_name))
     cursor.execute(adding_medals, (country_code, gold, silver, bronze))
     db.commit()
     print(f"{country_name} has been added to the table")
-def removing(): #Remove a country
+def removing_country(): #Remove a country
     country_code = input("Enter the country code to remove: ").upper()
     cursor.execute(deleting_medals, (country_code,))
     cursor.execute(deleting_country, (country_code,))
     db.commit()
     print(f"{country_code} has been removed from the table")
-def finding():#Find a country
+def finding_country():#Find a country
     country_code = input("Enter the country code to find: ").upper()
-    cursor.execute(finding, (country_code,))
+    cursor.execute(finding_country, (country_code,))
     country = cursor.fetchone()
     if country is not None:
         print(f"{'Code':<5}{'Country':<23}{'Gold':<6}{'Silver':<7}{'Bronze':<6}")
@@ -63,11 +62,11 @@ while True:
         for row in result:
             print(f"{row[0]:<5}{row[1]:<23}{row[3]:<6}{row[4]:<7}{row[5]:<6}")
     elif Choose == 2:
-        adding()
+        adding_country()
     elif Choose == 3:
-        removing()
+        removing_country()
     elif Choose == 4:
-        finding()
+        finding_country()
     elif Choose == 5:   
         print("Thank you for using the Olympic Medal Table. Goodbye!")
         break

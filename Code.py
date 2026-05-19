66bf67ec8cced8f7f649890307eeaa8edc5401ce
25688-cell <25688@burnside.school.nz>
2026-05-19 12:44:39 +1200

    Version 3.0: Fixed bugs and other tested boundaries

--- Code.py
+++ Code.py
@@ -7,7 +7,8 @@ adding_country = "INSERT INTO Country (country_code, country_name) VALUES (?, ?)
 adding_medals = "INSERT INTO Medals (country_code, gold, silver, bronze) VALUES (?, ?, ?, ?);"
 deleting_country = "DELETE FROM Country WHERE country_code = ?;"
 deleting_medals = "DELETE FROM Medals WHERE country_code = ?;"
-finding_country = "SELECT * FROM Country, Medals WHERE Country.country_code = ?;"
+finding = "SELECT * FROM Country, Medals WHERE Country.country_code = ?;"
+duplicate = "SELECT * FROM Country WHERE country_code = ? OR country_name = ?;"
 # -----------------------------------------------------
 db = sqlite3.connect(database)
 cursor = db.cursor()
@@ -18,7 +19,12 @@ print("Welcome to the Olympic Medal Table! What would you like to do?")
 def adding_country(): #Used to add a country to the medal table
     country_code = input("Country code: ").upper()
     country_name = input("Country name: ")
-    if not country_code.isalpha() or len(country_code) != 3 or not country_name.isalpha():
+    cursor.execute(duplicate, (country_code, country_name))
+    found = cursor.fetchone()
+    if found is not None:
+        print("Name or code already existed. Try again")
+        return
+    if not country_code.isalpha() or len(country_code) != 3 or not country_name.isalpha() :
         print("Invalid name or code. Country code must be 3 letters and country name must contain only letters")
         return
     try:
@@ -33,15 +39,15 @@ def adding_country(): #Used to add a country to the medal table
     cursor.execute(adding_medals, (country_code, gold, silver, bronze))
     db.commit()
     print(f"{country_name} has been added to the table")
-def removing_country(): #Remove a country
+def removing_country(): #Removing a country
     country_code = input("Enter the country code to remove: ").upper()
     cursor.execute(deleting_medals, (country_code,))
     cursor.execute(deleting_country, (country_code,))
     db.commit()
     print(f"{country_code} has been removed from the table")
-def finding_country():#Find a country
+def finding_country():#Finding a country
     country_code = input("Enter the country code to find: ").upper()
-    cursor.execute(finding_country, (country_code,))
+    cursor.execute(finding, (country_code,))
     country = cursor.fetchone()
     if country is not None:
         print(f"{'Code':<5}{'Country':<23}{'Gold':<6}{'Silver':<7}{'Bronze':<6}")
@@ -61,13 +67,13 @@ while True:
         print(f"{'Code':<5}{'Country':<23}{'Gold':<6}{'Silver':<7}{'Bronze':<6}")
         for row in result:
             print(f"{row[0]:<5}{row[1]:<23}{row[3]:<6}{row[4]:<7}{row[5]:<6}")
-    elif Choose == 2:
+    elif Choose == 2: #If the user wants to add a country  
         adding_country()
-    elif Choose == 3:
+    elif Choose == 3: #If the user wants to remove a country
         removing_country()
-    elif Choose == 4:
+    elif Choose == 4: #If the user wants to find a country
         finding_country()
-    elif Choose == 5:   
+    elif Choose == 5: #If the user wants to exit the program
         print("Thank you for using the Olympic Medal Table. Goodbye!")
         break
     else:
